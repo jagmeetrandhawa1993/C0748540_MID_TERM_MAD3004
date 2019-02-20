19cda85d5b1dcb19d5d4313ad1b5e8e7aade031e
Jagmeet Kaur <jagmeetkaur@jagmeets-macbook-air.local>
2019-02-20 18:29:08 -0500

    Initial Commit

---
+++ C0748540_MID_TERM_MAD3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  C0748540_MID_TERM_MAD3004
+//
+//  Created by Jagmeet Kaur on 2019-02-20.
+//  Copyright © 2019 Jagmeet Kaur. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
